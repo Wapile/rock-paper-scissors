ace092fab61354ec77afe07d49eeb5fb8c544b8e
Wapile <00trent@protonmail.com>
2021-06-09 13:04:43 -0500

    Restructured .js file to better nest functions/variables

--- js/main.js
+++ js/main.js
@@ -1,68 +1,68 @@
 
-// Create computerPlay function
-function computerPlay() {
-
-    // Create math random fuction returning number 0, 1 or 2
-    // Round number down to integer
-    randomizer = Math.floor(Math.random() * 3);
-
-    // Assign Rock/Paper/Scissor value based on returned integer
-    // Return result of random selection
-    if (randomizer === 0) {
-        
-        return "rock";
-
-    }
-
-    else if (randomizer === 1) {
-
-        return "paper";
-
-    }
-
-    else {
-
-        return "scissors";
-    }
-
-}
-
-// Create playerPlay function
-function playerPlay() {
-
-    // Prompt user for selection as string with no case sensitivity
-    let selection = prompt("Rock, Paper or Scissors?", "");
-    
-    // Return selection based on input
-    if (selection.toLowerCase() === "rock") {
-        return "rock";
-    } 
-
-    else if (selection.toLowerCase() === "paper") {
-        return "paper";
-    }
-
-    else if (selection.toLowerCase() === "scissors") {
-        return "scissors";
-    }
-
-    // Notify user if input is incorrect
-    else {
-        alert("Please check spelling and try again.")
-    }
-}
-
-
-// Set function returns to variables for later
-const computerSelection = computerPlay();
-const playerSelection = playerPlay();
-
-// Print computer and player selections
-console.log(`Computer Selection = ${computerSelection}`);
-console.log(`Your Selection = ${playerSelection}`);
 
 // Create playRound function
 function playRound() {
+
+
+        // Create computerPlay function
+        function computerPlay() {
+
+            // Create math random fuction returning number 0, 1 or 2
+            // Round number down to integer
+            randomizer = Math.floor(Math.random() * 3);
+
+            // Assign Rock/Paper/Scissor value based on returned integer
+            // Return result of random selection
+            if (randomizer === 0) {
+                return "rock";
+            }
+
+            else if (randomizer === 1) {
+                return "paper";
+            }
+
+            else {
+                return "scissors";
+            }
+        }
+
+
+        // Create playerPlay function
+        function playerPlay() {
+
+            // Prompt user for selection as string with no case sensitivity
+            let selection = prompt("Rock, Paper or Scissors?", "");
+            
+            // Return selection based on input
+            if (selection.toLowerCase() === "rock") {
+                return "rock";
+            } 
+
+            else if (selection.toLowerCase() === "paper") {
+                return "paper";
+            }
+
+            else if (selection.toLowerCase() === "scissors") {
+                return "scissors";
+            }
+
+            // Notify user if input is incorrect
+            else {
+                alert("Please check spelling and try again.")
+            }
+        }
+
+
+    // Set function returns to variables for later
+    const computerSelection = computerPlay();
+    const playerSelection = playerPlay();
+
+    // Print computer and player selections
+    console.log(`Computer Selection = ${computerSelection}`);
+    console.log(`Your Selection = ${playerSelection}`);
+    x = 0;
+    y = 0;
+
 
     // Compare computerSelection and playerSelection to determine winner
     // Return winner or tie
@@ -74,19 +74,24 @@ function playRound() {
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
-        ) {
-            return `You win! Your ${playerSelection} beats ${computerSelection}.`;
-        }
+    ) {
+        x = ++x;
+        return `You win! Your ${playerSelection} beats ${computerSelection}.`;
+    }
 
     else {
+        y = ++y;
         return `You lose. Your ${playerSelection} loses to ${computerSelection}.`;
     }
 
 }
 
 console.log(playRound());
-
+console.log(`CURRENT SCORE -- You: ${x}  Computer: ${y}`)
 
 // Create game function that runs playRound function x5 times
-// console.log result of each round
-// console.log final winner out of 5 rounds
+// Print result of each round
+// Set variables to track score
+// Print current score after each round
+// Compare scores after 5 rounds
+// Print final winner out of 5 rounds
