c87f91d7126ad547cd45b28be56337ed2caab6d4
Wapile <00trent@protonmail.com>
2021-06-09 11:01:41 -0500

    Added initial pseudo code and first randomizer script

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+// Create computerPlay function
+function computerPlay() {
+    randomizer = Math.floor(Math.random() * 3);
+}
+
+// Randomly select Rock, Paper or Scissors
+// Return result of random selection
+// Create playerPlay function
+// Prompt user for selection as string with no case sensitivity
+// Return selection as playerSelection variable
+// Create playRound function
+// Compare computerSelection and playerSelection to determine winner
+// Return result
+// Create game function that runs playRound function x5 times
+// console.log result of each round
+// console.log final winner out of 5 rounds
